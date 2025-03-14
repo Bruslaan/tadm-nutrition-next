b8af8305aceb6c781817dd041a130e5259f6c9dc
Ruslan Noschajew <bruslan.no@gmail.com>
2025-03-14 22:49:41 +0100

    i18 added

--- components/layout/navbar/mobile-menu.tsx
+++ components/layout/navbar/mobile-menu.tsx
@@ -3,11 +3,10 @@
 import { Dialog, Transition } from '@headlessui/react';
 import Link from 'next/link';
 import { usePathname, useSearchParams } from 'next/navigation';
-import { Fragment, Suspense, useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import { Menu } from 'lib/shopify/types';
-import Search, { SearchSkeleton } from './search';
 import LanguageSwitcher from '../../LanguageSwitcher';
 
 export default function MobileMenu({ menu }: { menu: Menu[] }) {
@@ -72,11 +71,11 @@ export default function MobileMenu({ menu }: { menu: Menu[] }) {
                   <XMarkIcon className="h-6" />
                 </button>
 
-                <div className="mb-4 w-full">
-                  <Suspense fallback={<SearchSkeleton />}>
-                    <Search />
-                  </Suspense>
-                </div>
+                {/*<div className="mb-4 w-full">*/}
+                {/*  <Suspense fallback={<SearchSkeleton />}>*/}
+                {/*    <Search />*/}
+                {/*  </Suspense>*/}
+                {/*</div>*/}
                 {menu.length ? (
                   <ul className="flex w-full flex-col">
                     {menu.map((item: Menu) => (
